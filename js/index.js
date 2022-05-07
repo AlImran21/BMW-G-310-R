91851e1d581b4f6b25a1fde95423edff2c3d206e
Al-Imran <alimran21feb@gmail.com>
2022-05-07 22:42:27 +0600

    added BMW G 310 R

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const redBtn = document.getElementById('red');
+const blueBtn = document.getElementById('blue');
+const blackBtn = document.getElementById('black');
+const bikeBtn = document.getElementById('bike');
+
+redBtn.onclick = function () {
+    bike.style.backgroundImage = "url(/images/BMW1.png)";
+};
+
+blueBtn.onclick = function () {
+    bike.style.backgroundImage = "url(/images/BMW2.png)";
+};
+
+blackBtn.onclick = function () {
+    bike.style.backgroundImage = "url(/images/BMW3.png)";
+};
